994c461cdd62875b9ce6b2d9e4f38f78795c9e16
omvishwakarma <om.panchal111@gmail.com>
2025-07-09 02:28:15 +0530

    get post explore

--- src/app.js
+++ src/app.js
@@ -2,10 +2,38 @@ const express = require('express');
 
 const app = express(); // create an express application
 
-app.use("/hello", (req, res)=>{
-    console.log('Request received');
-    res.send('response from server'); // send a response to the client
+
+// this will match with get methods only
+app.get('/user', (req, res)=>{
+    res.send({
+        name: 'John',
+        age: 20,
+        city: 'New York'
+    }); // send a response to the client
 });
+
+app.post('/user', (req, res)=>{
+    res.send('user created'); // send a response to the client
+});
+
+
+// order of routes matters
+// this will match with all http methods
+
+// app.use('/hello/2', (req, res)=>{
+//     res.send('abara ka dabra'); // send a response to the client
+// });
+
+// app.use("/hello", (req, res)=>{
+//     res.send('hello from server'); // send a response to the client
+// });
+
+// app.use("/", (req, res)=>{
+//     res.send('Home page from server'); // send a response to the client
+// });
+
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
