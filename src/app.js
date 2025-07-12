6bae6ba429ab14637843cd72f0001fc0cd133d7e
omvishwakarma <om.panchal111@gmail.com>
2025-07-12 11:24:01 +0530

    db connect and then listen port

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express'); 
-require('./config/database');
+const connectDB = require('./config/database');
 
 const app = express(); // create an express application
 
@@ -15,11 +15,19 @@ app.use('/user', [(req, res, next)=>{
     res.send('response from middleware 3'); // send a response to the client
 });
 
+connectDB().then(()=>{
+    console.log('Connected to MongoDB');
+    // start the server after connecting to the database
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+        // add a listener for the server to listen for requests
+    });
+}).catch((err)=>{
+    console.log('Error connecting to MongoDB', err);
+})
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-    // add a listener for the server to listen for requests
-});
+
+
 
 
 
--- src/config/database.js
+++ src/config/database.js
@@ -6,11 +6,9 @@ const connectDB = async () => {
   
 }
 
-connectDB().then(()=>{
-    console.log('Connected to MongoDB');
-}).catch((err)=>{
-    console.log('Error connecting to MongoDB', err);
-})
+module.exports = connectDB;
+
+
 
 
 
